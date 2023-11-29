8fa3152c38fab45e6d3827ef0daa99e55aff5c19
VadymKrem <122228240+VadymKrem@users.noreply.github.com>
2023-11-29 17:40:19 +0200

    start 29-11
    побудова структури проекта

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "components/App";
 import "./index.css";
-import { App } from "./components/App";
+import { Provider } from "react-redux";
+import { persistor, store } from "redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/project-rent-auto">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
