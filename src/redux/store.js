3b142e430ee81c94cfabd9f91dacd90a9ab4c33e
VadymKrem <122228240+VadymKrem@users.noreply.github.com>
2023-11-29 18:20:18 +0200

    comment redux for right deploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { configureStore } from "@reduxjs/toolkit";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
-import { authReducer } from "redux/auth/authSlice";
-import { contactsReducer } from "redux/catalog/catalogAutoSlice";
-import { filtersReducer } from "redux/filter/filterSlice";
+// import { authReducer } from "redux/auth/authSlice";
+// import { contactsReducer } from "redux/catalog/catalogAutoSlice";
+// import { filtersReducer } from "redux/filter/filterSlice";
 
-const authPersistConfig = {
-  key: "auth",
-  storage,
-  whitelist: ["token"],
-};
+// const authPersistConfig = {
+//   key: "auth",
+//   storage,
+//   whitelist: ["token"],
+// };
 
-export const store = configureStore({
-  reducer: {
-    auth: persistReducer(authPersistConfig, authReducer),
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  devTools: process.env.NODE_ENV === "developent",
-});
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistReducer(authPersistConfig, authReducer),
+//     contacts: contactsReducer,
+//     filters: filtersReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+//   devTools: process.env.NODE_ENV === "developent",
+// });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
