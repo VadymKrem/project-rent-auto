81c4212084870ce8e362c75709f2b223f1f4c59c
VadymKrem <122228240+VadymKrem@users.noreply.github.com>
2023-12-03 19:44:14 +0000

    Deploying to gh-pages from @ VadymKrem/project-rent-auto@ae2214aa00c29fa520358a3e93ee242c19bafbd5 🚀

--- static/js/733.1b0defe5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[733],{733:(e,t,a)=>{a.r(t),a.d(t,{default:()=>c});var s=a(184);const c=()=>(0,s.jsx)("div",{children:"Favorites"})}}]);
-//# sourceMappingURL=733.1b0defe5.chunk.js.map
